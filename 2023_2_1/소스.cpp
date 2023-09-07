5975d7549feaad6f5ff7120aedf3120c21568ee0
choenu <dlcksdn0428@gmail.com>
2023-09-07 15:34:42 +0900

    for

--- 2023_2_1/소스.cpp
+++ 2023_2_1/소스.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-	printf("Hello world!\n");
-	printf("Hello KNU!\n");
-	printf("Hello SW!\n");
+int main() {
+    int n;
 
-	return 0;
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i; j < n; ++j) {
+            printf(" ");
+        }
+        for (int j = 1; j <= (2 * i - 1); ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }

be34f9e3ef90d247b6db300ffcf77c48fe8b2573
choenu <dlcksdn0428@gmail.com>
2023-09-14 15:42:27 +0900

    jumsu

--- 0914_1/lecture6.c
+++ 0914_1/lecture6.c
@@ -4,12 +4,14 @@ int main()
 	int p;
 	printf("점수 입력 : ");
 	scanf_s("%d", &p);
+
 	if (p >= 90) printf("A 학점 입니다.");
 	else if (p >= 80) printf("B 학점 입니다.");
 	else if (p >= 70) printf("C 학점 입니다.");
 	else if (p >= 60) printf("D 학점 입니다.");
 	else if (p < 60) printf("F 학점 입니다.");
-	else if (p < 0 || p>100) printf("잘못 입력하였습니다.");
+	else if (p < 0 || p>100) 
+		printf("잘못 입력하였습니다.");
 
 	return 0;
 }

6a325a9c291ab79a802f66e773f43ae7471d26bd
choenu <dlcksdn0428@gmail.com>
2023-09-21 15:38:45 +0900

    Factorial

--- 0921/lecture3.c
+++ 0921/lecture3.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 
-int factorial(int num)
+int factorial(int num, int result)
 {
-    int result = 1;
-    for (int i = 1; i <= num; ++i)
-    {
-        result = result * i;
+    result = num * result;
+    num = num--;
+    if (num == 0) {
+        return result;
     }
-    return result;
+    else {
+        factorial(num, result);
+    }
 }
 
 int main(void) {
 
     int num = 0;
-    int result = 0;
+    int result = 1;
 
     printf_s("계산할 팩토리얼 값을 입력하시오. : ");
     scanf_s("%d", &num);
 
-    result = factorial(num);
+    result = factorial(num,result);
 
     printf_s("%d! = %d", num, result);
 
